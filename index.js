f592dee9a8f9d906ea8b6e738390ac15d438dd8c
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-03 01:47:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,103 @@
 function dwarfRollCall(dwarves) {
+  
+let string = "" ; 
+
+for (let i=0 ; i<dwarves.length ; i++){
+  
+string +=`${i+1}. ${dwarves[i]} `;
+  
 }
+  return string;
+}
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  let newArr = [];
+  
+  for(let i= 0 ; i< planeteerCalls.length; i++){
+    
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase();
+    
+    planeteerCalls[i] =planeteerCalls[i].concat("!");
+     
+     newArr.push(planeteerCalls[i]);
+  }
+ 
+  
+  return newArr;
 }
+
+
+
 
 function longPlaneteerCalls(words) {
+   
+   if(words.length>=4){
+   
+   return (true);
+   
+ }else{
+   
+   return (false);
+
 }
 
-function findTheCheese (foods) {
 }
+
+
+
+/*
+function findTheCheese (foods) {
+  
+  var count=foods.length;
+  
+  for (let i=0 ; i<foods.length ; i++){
+    
+          if(foods[i]==="cheddar"){
+    
+           return foods[i];
+  
+             }else if (foods[i]==="gouda"){
+  
+             return foods[i];
+  
+          }else if (foods[i]==="camembert"){
+  
+         return foods[i];
+         
+      }
+  
+     
+      }
+ return ("no cheese!");
+  }
+  */
+ 
+
+
+function findTheCheese (foods) {
+  
+  var  cheeses = ["cheddar","gouda","camembert"] 
+  
+   for (let i=0 ; i<foods.length ; i++){
+     
+   if (cheeses.includes(foods[i])){
+     
+     return foods[i];
+   }
+     
+   }
+   return "no cheese!";
+}
+
+
+
+
+
+
+
+
+
+
